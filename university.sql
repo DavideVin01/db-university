ca61a5d245a01b8759f4d08a92188d9ad9f7fd8e
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-09 18:01:40 +0100

    Completes 5th Point

--- university.sql
+++ university.sql
@@ -15,3 +15,9 @@ SELECT *, `period`, `year`
 FROM `courses`
 WHERE `period` = 'I semestre'
 AND `year` = 1
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT *, `hour`, `date`
+FROM `exams`
+WHERE `hour` LIKE '16%'
+AND `date` = '2020-06-20'
